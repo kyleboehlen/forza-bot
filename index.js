9d7720492a944968019298b88c9522fd92ad720b
kyleboehlen <kyleboehlen@gmail.com>
2022-01-11 15:41:54 -0700

    This is literally the whole bot

---
+++ index.js
@@ -0,0 +1,51 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const {clientId, guildId, token} = require('./config.json');
+
+const commands = [{
+  name: 'car',
+  description: 'Gives you a car from Forza'
+}]; 
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      // Routes.applicationGuildCommands(clientId, guildId), // For devlopment, register commands to guild immediately
+      Routes.applicationCommands(clientId),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const axios = require('axios');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'car') {
+    axios.get("https://forza-api.tk/").then((response) => {
+      image = response.data.image;
+      const embed = new MessageEmbed().setImage(image).setDescription('Here is a car from Forza:');
+      interaction.reply({
+        embeds: [embed],
+        ephemeral: false
+      });
+    });
+  }
+});
+
+client.login(token);
---
+++ readme.md
@@ -0,0 +1,18 @@
+This is baby's first Discord bot.
+
+Copy the config
+
+`cp config.example.json config.json`
+
+and fill out the token
+
+Simply run
+
+`yarn install`
+
+and
+
+`yarn run start`
+
+This bot only uses Discord.js with no boilerplate or framework
+https://discord.js.org/#/docs/discord.js/stable/general/welcome
